73e1a76619e9bee65ffe77fa3a26ebcb6f5249e9
Rouan van der Ende <rouan@netron.co.za>
2024-10-14 11:38:23 +0200

    x script

--- tools/docker/dockerfiles/x11/x.sh
+++ tools/docker/dockerfiles/x11/x.sh
@@ -3,7 +3,7 @@
 LOCKFILE=/tmp/.X0-lock
 
 # remove lockfile before launching X.
-
-rm -f $LOCKFILE ;
+rm -f /tmp/.X0-lock
+echo "STARTING X11"
 openbox &
-X -s 0 -quiet -dpms -nocursor -novtswitch vt2 -background none
+X -s 0 -dpms -nocursor -novtswitch vt2 -background none
